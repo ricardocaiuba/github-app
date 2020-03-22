b399092463c205f3d91539fc2bda0169b4120511
ricardocaiuba <ricardo.caiuba@gmail.com>
2020-03-22 19:38:23 -0300

    github-app --> M1#A36

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React from "react";
 
+import Search from "./Components/Search";
+import UserInfo from "./Components/User-Info";
+import Actions from "./Components/Actions";
+import Repos from "./Components/Repos";
+
 const App = () => {
   return (
     <div className="app">
-      <div className="search">
-        <input type="search" placeholder="Digite o nome do usuário no GitHub" />
-      </div>
-      <div className="user-info">
-        <img src="https://avatars3.githubusercontent.com/u/43193614?v=4" />
-        <h1>
-          <a href="https://github.com/ricardocaiuba">Ricardo Rodrigues</a>
-        </h1>
-        <ul className="repos-info">
-          <li>Repositórios: 24</li>
-          <li>Seguidores: 2</li>
-          <li>Seguindo: 2</li>
-        </ul>
-        <hr />
-        <div className="actions">
-          <button>Ver repositórios</button>
-          <button>Ver favoritos</button>
-        </div>
-        <hr />
-        <div className="repos">
-          <h2>Repositórios</h2>
-          <ul>
-            <li>
-              <a href="#">Nome do repositório</a>
-            </li>
-          </ul>
-        </div>
-        <hr />
-        <div className="starred">
-          <h2>Favoritos</h2>
-          <ul>
-            <li>
-              <a href="#">Nome do repositório</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Search />
+      <UserInfo />
+      <Actions />
+      <Repos
+        className="repos"
+        title="Repositórios:"
+        repos={[
+          {
+            name: "Nome do Repositório",
+            link: "#"
+          },
+          {
+            name: "Nome do Repositório 2",
+            link: "#"
+          }
+        ]}
+      />
+      <Repos
+        className="starred"
+        title="Favoritos:"
+        repos={[
+          {
+            name: "Nome do Repositório",
+            link: "#"
+          },
+          {
+            name: "Nome do Repositório 2",
+            link: "#"
+          },
+          {
+            name: "Nome do Repositório 3",
+            link: "#"
+          }
+        ]}
+      />
     </div>
   );
 };
